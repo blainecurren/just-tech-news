833d56adac23c3a3be234f3d0490096c67b72bb9
Blaine Curren <blaine.curren@gmail.com>
2021-08-25 10:05:40 -0400

    User.js redo

--- models/User.js
+++ models/User.js
@@ -7,13 +7,13 @@ class User extends Model {}
 // define table columns and configuration
 User.init(
   {
-    // Define an id column
+    // define an id column
     id: {
       // use the special Sequelize DataTypes object provide what type of data it is
       type: DataTypes.INTEGER,
       // this is the equivalent of SQL's `NOT NULL` option
       allowNull: false,
-      // instruct that this is the Primary key
+      // instruct that this is the Primary Key
       primaryKey: true,
       // turn on auto increment
       autoIncrement: true,
@@ -39,23 +39,16 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        // this means the password must be at least four char's
+        // this means the password must be at least four characters long
         len: [4],
       },
     },
   },
   {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-    // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
-    // don't pluralize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
-    // make it so our model name stays lowercase in the database
     modelName: "user",
   }
 );

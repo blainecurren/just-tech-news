b5ecbc9f281134d2350928037d690afb1523a80d
Blaine Curren <blaine.curren@gmail.com>
2021-09-09 17:37:57 -0500

    added logout btn

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -44,4 +44,16 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/login");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/", (req, res) => {
+  console.log(req.session);
+});
 module.exports = router;

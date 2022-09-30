449f7854e678384f8f2de5a8f1439b86d721deb1
Blaine Curren <blaine.curren@gmail.com>
2022-09-30 17:10:57 -0500

    connection setup and dotenv installed

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
-// Import the Sequelize constructor from the library
+// import the Sequelize constructor from the library
 const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-// create connection to our database, pass in your MYSQL info for username and password
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    });
+// create connnection to our databse, pass in your mysql info for username and password
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.USERNAME,
+  process.env.PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
